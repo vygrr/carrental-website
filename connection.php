dbc4b62240c57e2d5b3da58190e4b88b0c3e2f91
dheeraj devadas! <60442254+vygrr@users.noreply.github.com>
2025-06-24 12:31:39 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection details (replace with your actual credentials)
 $servername = "localhost";
-$username = "root"; // MySQL User Name
-$password = ""; // MySQL Password
+$username = "your_username"; // MySQL User Name
+$password = "your_password"; // MySQL Password
 $dbname = "user"; // MySQL DB Name
 
 // Create connection
